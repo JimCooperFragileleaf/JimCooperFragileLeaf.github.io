0b1cd322250131a644dc4386588a82c88e8a9fd4
JimCooperFragileleaf <Jim@Andrews-MacBook-Pro.local>
2016-10-24 23:55:24 +0200

    Bound calculation to change events

--- client.js
+++ client.js
@@ -4,17 +4,27 @@
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
 
-$(function() {
-  console.log('hello world :o');
-  
-
+$(function () {
   $('form').submit(function(event) {
     event.preventDefault();
-    var numberOfLeftovers = document.getElementById("numberOfLeftovers").value;
-    var multiplier=document.getElementById("foodstuff").value;
-    var co2=Math.abs(numberOfLeftovers) * multiplier;
-    var km=co2 / 0.16;
-    document.getElementById('co2Result').innerHTML = co2.toFixed(3) + ' kg of CO<sub>2</sub><br>' + 
-                                                     km.toFixed(2) + ' km in an average car';
-    });
+    CalculateFootprints();
+  });
+  
+  $('#numberOfLeftovers').on('input', function() { 
+    CalculateFootprints();
+  });
+    
+  $('#foodstuff').on('change',function(){
+    CalculateFootprints();
+  });
+    
+  function CalculateFootprints()
+    {
+      var numberOfLeftovers = document.getElementById("numberOfLeftovers").value;
+      var multiplier=document.getElementById("foodstuff").value;
+      var co2=Math.abs(numberOfLeftovers) * multiplier;
+      var km=co2 / 0.16;
+      document.getElementById('co2Result').innerHTML = co2.toFixed(3) + ' kg of CO<sub>2</sub><br>' + 
+                                                       km.toFixed(2) + ' km in an average car';
+    }
 });
