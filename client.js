fe6af82cd9386230793925cfe247f2575faf5b1c
JimCooperFragileleaf <Jim@Andrews-MacBook-Pro.local>
2016-10-25 17:10:51 +0200

    Changed UI to table

--- client.js
+++ client.js
@@ -1,25 +1,23 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
 $(function () {
-  $('#numberOfLeftovers').on('input', function() { 
-    CalculateFootprints();
-  });
+  CalculateFootprints();
     
-  $('#foodstuff').on('change',function(){
-    CalculateFootprints();
-  });
+  $('body').on('change', '.numberInput', CalculateFootprints);
     
   function CalculateFootprints()
     {
-      var numberOfLeftovers = document.getElementById("numberOfLeftovers").value;
-      var multiplier=document.getElementById("foodstuff").value;
-      var co2=Math.abs(numberOfLeftovers) * multiplier;
+        
+      var broodjesVlees = Math.abs($("#broodjesVlees").val());
+      var broodjesKaas = Math.abs($("#broodjesKaas").val());
+      var banaans = Math.abs($("#banaan").val());
+      var koffies = Math.abs($("#koffie").val());
+    
+      var co2 = (broodjesVlees * 0.46) +
+                (broodjesKaas * 0.37) +
+                (banaans * 0.05) +
+                (koffies * 0.07);
       var km=co2 / 0.16;
-      document.getElementById('co2Result').innerHTML = co2.toFixed(3) + ' kg of CO<sub>2</sub><br>' + 
+      
+      document.getElementById('co2Result').innerHTML = co2.toFixed(2) + ' kg of CO<sub>2</sub><br>' + 
                                                        km.toFixed(2) + ' km in an average car';
     }
 });
