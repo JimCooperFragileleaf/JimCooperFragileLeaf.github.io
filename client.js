35094d2ede5f56ddb1e0c227df4da057bfa15bea
JimCooperFragileleaf <Jim@Andrews-MacBook-Pro.local>
2016-10-26 09:53:26 +0200

    Changed multipliers

--- client.js
+++ client.js
@@ -6,10 +6,11 @@ $(function () {
   function CalculateFootprints() {
     var rowNumbers = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 14, 15];
     var inputIds = ['pork', 'beef', 'chicken', 'salmon', 'cheese', 'orangeJuice', 'milk', 'coffee', 'apple', 'pear', 'clementine', 'orange', 'banana'];
-    var co2Multipliers = [0.1905, 0.45575, 0.13325, 0.1785, 0.3696, 0.668, 1.062, 0.273, 0.0325, 0.0423, 0.0144, 0.048, 0.046];
-    var waterMultipliers = [149.7, 385.375, 108.125, 0, 151.8, 1018, 1020, 0, 102.75, 130.002, 33.66, 0, 0];
+    var co2Multipliers = [0.1905, 0.45575, 0.13325, 0.1785, 0.3696, 0.167, 0.2655, 0.06825, 0.0325, 0.0423, 0.0144, 0.048, 0.046];
+    var waterMultipliers = [149.7, 385.375, 108.125, 0, 151.8, 254.5, 255, 0, 102.75, 130.002, 33.66, 0, 0];
     const co2DPs = 2;
     const waterDPs = 0;
+    const percDPs = 1;
     const co2Column = 2;
     const co2PercColumn = 3;
     const waterColumn = 4;
@@ -18,10 +19,10 @@ $(function () {
     var totalCO2 = GetTotal(inputIds, co2Multipliers, co2DPs).toFixed(co2DPs);
     var km = (totalCO2 / 0.16).toFixed(co2DPs);
     var totalWater = GetTotal(inputIds, waterMultipliers, waterDPs).toFixed(waterDPs);
-    var bathTubs = (totalWater / 80).toFixed(waterDPs);
+    var bathTubs = (totalWater / 80).toFixed(1);
       
-    SetTableValues(co2Column, co2PercColumn, totalCO2, rowNumbers, inputIds, co2Multipliers, co2DPs);
-    SetTableValues(waterColumn, waterPercColumn, totalWater, rowNumbers, inputIds, waterMultipliers, waterDPs);
+    SetTableValues(co2Column, co2PercColumn, totalCO2, rowNumbers, inputIds, co2Multipliers, co2DPs, percDPs);
+    SetTableValues(waterColumn, waterPercColumn, totalWater, rowNumbers, inputIds, waterMultipliers, waterDPs, percDPs);
     
     document.getElementById('co2Result').innerHTML = totalCO2 + ' kg of CO<sub>2</sub><br>' + 
                                                      km + ' km in an average car<br><br>' +
@@ -29,10 +30,10 @@ $(function () {
                                                      bathTubs + ' bathtubs';
     }
   
-  function SetTableValues(column, percColumn, total, rowNumbers, inputIds, multipliers, decimalPoints) {
+  function SetTableValues(column, percColumn, total, rowNumbers, inputIds, multipliers, decimalPoints, percDecimalPoints) {
      for (i = 0; i < rowNumbers.length; i++) {
          var value = CalculateValueFor(inputIds[i], multipliers[i]).toFixed(decimalPoints);
-         var percentage = total == 0 ? '' : (100 * value / total).toFixed(decimalPoints);
+         var percentage = total == 0 ? '' : (100 * value / total).toFixed(percDecimalPoints);
          SetCellValue(rowNumbers[i], column, value);
          SetCellValue(rowNumbers[i], percColumn, percentage);
      } 
